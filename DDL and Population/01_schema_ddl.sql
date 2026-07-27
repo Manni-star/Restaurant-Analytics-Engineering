c2eb7c6731bd94bbb87e29f0dfe3a4ac3005d129
Manvesh <Manni-star@://github.com>
2026-07-27 17:33:36 -0400

    feat: create ingredients, recipe, inventory and customers base tables

--- DDL and Population/01_schema_ddl.sql
+++ DDL and Population/01_schema_ddl.sql
@@ -27,3 +27,43 @@ CREATE TABLE `items` (
     )
 );
 
+CREATE TABLE `ingredients` (
+    `ing_id` varchar(70)  NOT NULL ,
+    `ing_name` varchar(50)  NOT NULL ,
+    `ing_weight` int  NOT NULL ,
+    `ing_meas` varchar(10)  NOT NULL ,
+    `ing_price` int  NOT NULL ,
+    PRIMARY KEY (
+        `ing_id`
+    )
+);
+
+CREATE TABLE `recipe` (
+    `row_id` int  NOT NULL ,
+    `sku` varchar(20)  NOT NULL ,
+    `ingredients` varchar(70)  NOT NULL ,
+    `quantity_value` int  NOT NULL ,
+    `quantity_unit` varchar(20)  NOT NULL ,
+    PRIMARY KEY (
+        `row_id`
+    )
+);
+
+CREATE TABLE `inventory` (
+    `inv_id` varchar(20)  NOT NULL ,
+    `ing_id` varchar(70)  NOT NULL ,
+    `stock_levels` int  NOT NULL ,
+    `reorder_point_packages` int  NOT NULL ,
+    PRIMARY KEY (
+        `inv_id`
+    )
+);
+
+CREATE TABLE `customers` (
+    `cust_id` int  NOT NULL ,
+    `cust_firstname` varchar(50)  NOT NULL ,
+    `cust_lastname` varchar(50)  NOT NULL ,
+    PRIMARY KEY (
+        `cust_id`
+    )
+);
