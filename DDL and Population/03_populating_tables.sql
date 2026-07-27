8283571f5a493306f83f0d5ae1215ccefcb6ea5c
Manvesh <Manni-star@://github.com>
2026-07-27 19:05:59 -0400

    feat: Created ETL staging tables stg_customers, stg_address, stg_items, stg_inventory, stg_shift and stg_staff for secure data ingestion

--- DDL and Population/03_populating_tables.sql
+++ DDL and Population/03_populating_tables.sql
@@ -4,3 +4,38 @@
 ------------------------
 USE restaurant_operations_analytics;
 
+---------------------------------
+-- STEP 1 — CREATE STAGING TABLES
+---------------------------------
+
+CREATE TABLE stg_customers AS
+SELECT *
+FROM customers
+WHERE 1 = 0;
+
+SHOW CREATE TABLE stg_customers;
+
+CREATE TABLE stg_address AS
+SELECT *
+FROM address
+WHERE 1 = 0;
+
+CREATE TABLE stg_items AS
+SELECT *
+FROM items
+WHERE 1 = 0;
+
+CREATE TABLE stg_inventory AS
+SELECT *
+FROM inventory
+WHERE 1 = 0;
+
+CREATE TABLE stg_shift AS
+SELECT *
+FROM shift
+WHERE 1 = 0;
+
+CREATE TABLE stg_staff AS
+SELECT *
+FROM staff
+WHERE 1 = 0;
